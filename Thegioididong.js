a376615ae82ae2288acadcc1770dd3320edf03c0
Thanghoangdz <hoangtrungthang2003@gmail.com>
2023-10-01 12:26:47 +0700

    submenu and adress-form 1

---
+++ Thegioididong.js
@@ -0,0 +1,12 @@
+// adress form btn
+
+const adressbtn = document.querySelector("#adress-form")
+const adressclose = document.querySelector("#adress-close")
+
+adressbtn.addEventListener("click", function() {
+    document.querySelector('.adress-form').style.display = "flex"
+})
+
+adressclose.addEventListener("click", function() {
+    document.querySelector('.adress-form').style.display = "none"
+})
