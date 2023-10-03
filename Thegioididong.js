d6326c0b8b7ed4073d55f727622c14a472eddcd0
Thanghoangdz <hoangtrungthang2003@gmail.com>
2023-10-04 01:38:07 +0700

    Thang-page-details-finished

--- Thegioididong.js
+++ Thegioididong.js
@@ -10,3 +10,16 @@ adressbtn.addEventListener("click", function() {
 adressclose.addEventListener("click", function() {
     document.querySelector('.adress-form').style.display = "none"
 })
+
+// Hiển thị số trang
+document.addEventListener('DOMContentLoaded', function() {
+    var carousel = document.getElementById('details');
+    var pageIndicator = document.getElementById('pageIndicator');
+
+    // Lắng nghe sự kiện khi carousel chuyển slide
+    carousel.addEventListener('slid.bs.carousel', function(event) {
+        var index = event.to + 1;
+        var total = event.target.querySelectorAll('.carousel-item').length;
+        pageIndicator.innerText = '(' + index + '/' + total + ')';
+    });
+});
